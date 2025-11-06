6a0c7c5f7af79c1e899f7ba6af7fdd78de5e4981
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2025-11-06 00:36:08 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom"; // 👈 usa HashRouter aquí
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -24,8 +24,8 @@ const App = () => (
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          {/* <Route path="/blog" element={<Blog />} /> */}
-          {/* <Route path="/blog/:id" element={<BlogPost />} /> */}
+          {/*<Route path="/blog" element={<Blog />} />*/}
+          {/*<Route path="/blog/:id" element={<BlogPost />} />*/}
           <Route path="/contact" element={<Contact />} />
           <Route path="/coming-soon" element={<ComingSoon />} />
           <Route path="*" element={<NotFound />} />
